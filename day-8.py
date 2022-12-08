83d9038e07527232732084a0345cb3192f2cb7d5
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-12-08 09:36:32 +0000

    DAY8-SECTION1-REIMPLEMENTATION

--- day-8.py
+++ day-8.py
@@ -8,38 +8,25 @@ def parseInput(filename):
     f.close()
     return lines
 
-def canBeSeen(grid, i, j):
-    x, y = i, j
-    res = True
-    while i - 1 >= 0:
-        res = res and (grid[x][y] > grid[i - 1][j])
-        if not res: break
-        i -= 1
-    if res: return res
+def isInBound(grid, p, q):
+    return 0 <= p < len(grid) and 0 <= q < len(grid[0])
 
-    res = True
-    i, j = x, y
-    while i + 1 < len(grid):
-        res = res and (grid[x][y] > grid[i + 1][j])
-        if not res: break
-        i += 1
-    if res: return res
-    res = True
+def canBeSeen(grid, i, j, x, y, ACTION):
+    if not isInBound(grid, x, y):
+        return True
 
-    i, j = x, y
-    while j - 1 >= 0:
-        res = res and (grid[x][y] > grid[i][j - 1])
-        if not res: break
-        j -= 1
-    if res: return res
-    res = True
+    initialCondition = (i == x) and (j == y)
+    if not initialCondition and grid[i][j] <= grid[x][y]:
+        return False
 
-    i, j = x, y
-    while j + 1 < len(grid[0]):
-        res = res and (grid[x][y] > grid[i][j + 1])
-        if not res: break
-        j += 1
-    return res
+    if ACTION == "RIGHT":
+        return canBeSeen(grid, i, j, x + 1, y, ACTION)
+    elif ACTION == "LEFT":
+        return canBeSeen(grid, i, j, x - 1, y, ACTION)
+    elif ACTION == "UP":
+        return canBeSeen(grid, i, j, x, y - 1, ACTION)
+    else:
+        return canBeSeen(grid, i, j, x, y + 1, ACTION)
 
 def scoreForTree(grid, i, j):
     scores = 1
@@ -85,13 +72,20 @@ def scoreForTree(grid, i, j):
 
 def computeNumberOfVisibleTrees(grid):
     count = len(grid) * 2 + (len(grid[0]) - 2) * 2
+    DIRECTIONS = ["UP", "DOWN", "LEFT", "RIGHT"]
+
+    def isTreeSeen(i, j):
+        seen = False
+        for dir in DIRECTIONS:
+            seen = seen or canBeSeen(grid, i, j, i, j, dir)
+        return seen
+
     for i in range(1, len(grid) - 1):
         for j in range(1, len(grid[0]) - 1):
-            if canBeSeen(grid, i, j):
+            seen = isTreeSeen(i, j)
+            if seen:
                 count += 1
     return count
-
-
 
 def computeMaxScenicScore(grid):
     _max = float('-inf')
