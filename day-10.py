b6c19043aac8b4b4b26e8f63ec5f4e52b42e96f7
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-12-13 05:10:09 +0000

    DAY10-SECTION1

--- day-10.py
+++ day-10.py
@@ -1,65 +1,48 @@
 from collections import defaultdict
 
 STRENGHTS = [i + 20 for i in range(0, 221, 40)]
-X = 1 # initial value in ragister
-cycle = 1
-strength = 1
-Queue = []
-res = 0
 
 def parseInput(filename):
     with open(filename) as f:
-        line = f.readline()
-        while line:
-            getRegisterValAtCycles(line)
-            line = f.readline()
+        lines = f.readlines()
+    return lines
 
-def getRegisterValAtCycles(line):
-    global cycle, X, res, Queue, strength
-    line = line.split(' ')
-    ins, val = None, None
-    if len(line) == 2:
-        ins, val = line[0].strip('\n'), line[1]
-    elif len(line) == 1:
-        ins, val = line[0].strip('\n'), str(0)
+def getRegisterValAtCycles(lines):
+    X = 1 # initial value in ragister
+    cycle = 0
+    res = 0
 
-    # store new instruction
-    if 'addx' in ins:
-        Queue.append([2, int(val)])
+    for line in lines:
+        line = line.split(' ')
+        ins = line[0]
+        val = None if len(line) <= 1 else line[1].replace('\n', '')
+        if 'noop' in ins:
+            cycle += 1
+            if cycle in STRENGHTS:
+                res += cycle * X
 
-    print(cycle, X, Queue[0])
-    if cycle in STRENGHTS:
-        res += X * cycle
+        elif 'addx' in ins:
+            val = int(val)
+            X += val
+            cycle += 1
 
-    # process existing instruction
-    if Queue:
-        Queue[0][0] -= 1
-        if Queue[0][0] <= 0:
-            X += Queue[0][1]
-            Queue.pop(0)
+            if cycle in STRENGHTS:
+                res += cycle * (X - val)
 
-    cycle += 1
+            cycle += 1
 
-        
-def resolveQueue():
-    global cycle, res, X, Queue , strength
-    while Queue:
-        if cycle in STRENGHTS:
-            res += X * cycle
-        print(cycle, X, X*cycle)
-        Queue[0][0] -= 1
-        if Queue[0][0] <= 0:
-            X += Queue[0][1]
-            Queue.pop(0)
+            if cycle in STRENGHTS:
+                res += cycle * (X - val)
 
-        cycle += 1
+        else: 
+            assert(False, "unknown token")
+    return res
     
 def day10():
-    global res
     filename = 'day-10.txt'
-    parseInput(filename)
-    resolveQueue()
-    yield None
+    lines = parseInput(filename)
+    res = getRegisterValAtCycles(lines)
+    yield res
 
 def main():
     for sol in day10():
